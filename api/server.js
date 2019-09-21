22d1b7ac26a3118c4e4d1a582812b5112c867972
Lucas Larrubia <Lucas@Marcias-iMac.local>
2019-09-20 22:28:11 -0400

    Routes need to be fixed

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config();
 
+const userRouter = require('../routes/userRouter');
+const exerciseRouter = require('../routes/exerciseRouter');
+
 console.log(process.env.JWT_SECRET);
 
 const server = express();
@@ -11,8 +14,8 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use('/api/', )
-// server.use('/api/exercises', )
+server.use('/api/', userRouter);
+server.use('/api/exercises', exerciseRouter);
 
 server.get('/', (req, res) => {
     res.send("YOOOO");
